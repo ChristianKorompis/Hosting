59ca6e24f5fe087034f7c60bfe4e4105ecb29e41
ChristianKorompis <95670457+ChristianKorompis@users.noreply.github.com>
2022-02-06 13:35:19 +0800

    Create core.js

---
+++ core.js
@@ -0,0 +1,44 @@
+(function ($) {
+
+	"use strict";
+
+	/**
+	 * Add 'mobile' class on Responsive Mode
+	 * @type {Window}
+	 */
+	$(window).on('load resize', function () {
+		var viewportWidth = window.outerWidth;
+		;
+
+		var siteHeader = $('.site-header');
+    
+    var isMobile = siteHeader.hasClass('mobile');
+    
+    var sitePost = $('#main');
+
+    var isMobilepost = siteHeader.hasClass('penggaya-post-dalam');
+    
+    if (viewportWidth < 1008) {
+			if (!isMobilepost) {
+				sitePost.addClass('penggaya-post-dalam');
+			}
+		} else {
+			if (isMobilepost) {
+				sitePost.removeClass('penggaya-post-dalam');
+			}
+		}
+
+		if (viewportWidth < 1008) {
+			if (!isMobile) {
+				siteHeader.addClass('mobile');
+				$('body').addClass('mobile');
+			}
+		} else {
+			if (isMobile) {
+				siteHeader.removeClass('mobile');
+				$('body').removeClass('mobile');
+			}
+		}
+	});
+})(jQuery);
+;
